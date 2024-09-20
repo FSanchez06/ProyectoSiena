927d62b17d6969f441a61f01eca06fc745a75e75
Aprendiz <felipesanchezcon2@gmail.com>
2024-09-20 12:19:09 -0500

    Se agrego modelo 3d de ejemplo

--- Front/src/pages/Design/Design.js
+++ Front/src/pages/Design/Design.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, AccumulativeShadows, RandomizedLight, Environment, CameraControls } from '@react-three/drei'
 
-const Journal = () => {
+const Desing = () => {
   const location = useLocation();
   const [prevLocation, setPrevLocation] = useState("");
   useEffect(() => {
@@ -10,22 +12,38 @@ const Journal = () => {
   }, [location]);
   return (
     <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title="Journals" prevLocation={prevLocation} />
-      <div className="pb-10">
-        <h1 className="max-w-[600px] text-base text-lightText mb-2">
-          <span className="text-primeColor font-semibold text-lg">Orebi</span>{" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-          reiciendis delectus vitae, aliquid sit iure dolorum commodi eum
-          numquam voluptate!
-        </h1>
-        <Link to="/shop">
-          <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
-            Continue Shopping
-          </button>
-        </Link>
+      <Breadcrumbs title="Diseño 3D" prevLocation={prevLocation} />
+      <div className="pb-50 h-[100vh] w-[140vh]">
+      <Canvas shadows camera={{ position: [5, 0, 5], fov: 35 }}>
+      <ambientLight intensity={Math.PI} />
+      <Shoe position={[0, 0, 0.85]} scale={10}/>
+      <Shoe position={[0, 0, -0.85]} rotation={[0, 0.5, Math.PI]} scale={-1} />
+      <AccumulativeShadows position={[0, -0.5, 0]} temporal frames={100} alphaTest={0.75} opacity={0.9}>
+        <RandomizedLight radius={6} position={[5, 5, -10]} bias={0.001} />
+      </AccumulativeShadows>
+      <CameraControls />
+      <Environment preset="city" />
+    </Canvas>
       </div>
     </div>
   );
 };
 
-export default Journal;
+function Shoe(props) {
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group {...props} dispose={null}>
+      <mesh castShadow receiveShadow geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  )
+}
+
+
+export default Desing;
