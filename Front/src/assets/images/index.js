78188286fc003b741b2439281f93acd953772372
unknown <felipesanchezcon2@gmail.com>
2024-09-20 01:33:15 -0500

    Aun falta actualizar interfaces pero se adelanto bastante

---
+++ Front/src/assets/images/index.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import logoLight from "./logoLight.png";
+
+// ============== Products Start here ================
+// ============== Products End here ======================
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  // ===================== Products start here ============
+  emptyCart,
+};
